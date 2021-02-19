1fcdda5e21e7586e57d7bb52b28aed54b2e1e9a7
Aleksey <alekseyhonchar@gmail.com>
2021-02-19 20:16:56 +0200

    Added a student constructor

--- lab2/lab2/Student.cs
+++ lab2/lab2/Student.cs
@@ -1,9 +1,21 @@
 ﻿namespace lab2
 {
-    class Student
+    public class Student
     {
-        string name;
-        int[] marks;
-        bool donater;
+        private string Name;
+        private int[] Marks = new int[5];
+        private bool Donater;
+        
+        public Student(string name, int mark1, int mark2, int mark3, int mark4, int mark5, bool donater)
+        {
+            Name = name;
+            Marks[0] = mark1;
+            Marks[1] = mark2;
+            Marks[2] = mark3;
+            Marks[3] = mark4;
+            Marks[4] = mark5;
+            Donater = donater;
+        }
+
     }
 }

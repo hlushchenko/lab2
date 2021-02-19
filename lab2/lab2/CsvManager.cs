4779f346dc7ac0b808d69a03292dfd56a37f2dd5
Hluushchenko <hluushchenko@gmail.com>
2021-02-19 20:19:21 +0200

    constructor added

--- lab2/lab2/CsvManager.cs
+++ lab2/lab2/CsvManager.cs
@@ -2,6 +2,10 @@ namespace lab2
 {
     public class CsvManager
     {
-        
+        private string _path;
+        public CsvManager(string path)
+        {
+            _path = path;
+        }
     }
 }

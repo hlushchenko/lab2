2498f33dbd5e5d50a6783feebcd75f6b950b62f7
Hluushchenko <hluushchenko@gmail.com>
2021-02-19 20:25:06 +0200

    refactoring

--- lab2/lab2/Student.cs
+++ lab2/lab2/Student.cs
@@ -2,19 +2,19 @@
 {
     public class Student
     {
-        private string Name;
-        private int[] Marks = new int[5];
-        private bool Donater;
+        private string _name;
+        private int[] _marks = new int[5];
+        private bool _donater;
         
         public Student(string name, int mark1, int mark2, int mark3, int mark4, int mark5, bool donater)
         {
-            Name = name;
-            Marks[0] = mark1;
-            Marks[1] = mark2;
-            Marks[2] = mark3;
-            Marks[3] = mark4;
-            Marks[4] = mark5;
-            Donater = donater;
+            _name = name;
+            _marks[0] = mark1;
+            _marks[1] = mark2;
+            _marks[2] = mark3;
+            _marks[3] = mark4;
+            _marks[4] = mark5;
+            _donater = donater;
         }
 
     }

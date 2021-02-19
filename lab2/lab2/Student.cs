7d30b4b48eecd18e92d53ba500dfe4b4ee278f4e
Aleksey <alekseyhonchar@gmail.com>
2021-02-19 20:54:08 +0200

    Now an average mark is calculated by a function; comments added

--- lab2/lab2/Student.cs
+++ lab2/lab2/Student.cs
@@ -4,22 +4,19 @@ namespace lab2
 {
     public class Student
     {
-        private string _name;
-        private int[] _marks = new int[5];
-        private bool _donater;
-        
-        public Student(string name, int mark1, int mark2, int mark3, int mark4, int mark5, bool donater)
+        private string _name; //the name of the student
+        public double averageMark; //the average student's mark 
+        private bool _donater; //checks if the student is a contractor
+
+
+        public Student(string name, int[] marks, bool donater) //students constructor :)
         {
             _name = name;
-            _marks[0] = mark1;
-            _marks[1] = mark2;
-            _marks[2] = mark3;
-            _marks[3] = mark4;
-            _marks[4] = mark5;
+            averageMark = calculateAverage(marks);
             _donater = donater;
         }
-
-        private static double calculateAverage(int[] data)
+        
+        private static double calculateAverage(int[] data) //this function was created to calculate the average value of marks :)
         {
             double result = 0;
             int sum = 0;
@@ -31,5 +28,7 @@ namespace lab2
 
             return result;
         }
+
+        
     }
 }

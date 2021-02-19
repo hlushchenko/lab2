50d6591f5efbd7103e66d476e0918b6bb4d5f1da
Aleksey <alekseyhonchar@gmail.com>
2021-02-19 21:51:36 +0200

    Utilities.cs added; toMatrix function moved to Utilities.cs

--- lab2/lab2/Student.cs
+++ lab2/lab2/Student.cs
@@ -59,12 +59,5 @@ namespace lab2
             return matrix;
         }
 
-        private static void outMatrix(string[,] matrix) //this function is printing the name of the student and his average mark :)
-        {
-            for (int i = 0; i < matrix.Length/2; i++)
-            {
-                Console.WriteLine($"{matrix[i, 0]}'s average mark is {matrix[i, 1]}");
-            }
-        }
     }
 }
